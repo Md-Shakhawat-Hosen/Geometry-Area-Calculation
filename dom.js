c9b254872f5a7e3f4d9376a97fa6b6aad0495874
Md-Shakhawat-Hosen <shakhawat.hosen.nsu@gmail.com>
2023-08-18 11:56:08 +0600

    when you calculate a geometry, it added only once when you submit

--- dom.js
+++ dom.js
@@ -15,26 +15,35 @@ function setCalculationValue(areaId,val) {
       area.innerText = val;
 }
 
+const arr = [];
 
 
-let c = 0;
 function finalCalculationResult(finalCal,name,val) {
-     c= c + 1;
+  
     const final = document.getElementById(finalCal);
 
     const nameText = document.getElementById(name)
+  
     const div = document.createElement('div');
 
-    div.innerHTML = `
+    if (arr.includes(nameText.innerText)) {
+        alert("You can't add twice")
+    }
+    else {
+      div.innerHTML = `
     <div class="flex justify-between mb-3">
-                             <h1><span>${c}.</span> ${nameText.innerText}</h1>
+                             <h1><span>${arr.length+1}.</span> ${nameText.innerText}</h1>
                              <h2><span>${val}</span>cm<sup>2</sup> </h2>
                              <button class="bg-blue-600 px-2 py-2 rounded-md text-white">convert to m<sup>2</sup></button>
                          </div>
     
     `;
 
-    final.appendChild(div);
+      final.appendChild(div);
+
+      arr.push(nameText.innerText);
+    }
+    
 }
 
 
